f5e68a62a706fe8018664c1296ec0dba2f72328c
Daniel Jones <tortxof@gmail.com>
2015-01-10 19:24:53 -0500

    Finalized style for newsletter pdf. Added next meeting date and subtitle.

--- htmltopdf/gen_pdf.py
+++ htmltopdf/gen_pdf.py
@@ -4,8 +4,8 @@ import sys
 import subprocess
 
 if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print("Usage: gen_pdf.py ../_posts/post.md ../newsletters/newsletter.pdf 'Date String'")
+    if len(sys.argv) < 5:
+        print("Usage: gen_pdf.py ../_posts/post.md ../newsletters/newsletter.pdf 'date string' 'next meeting date string'")
         sys.exit()
     post_file = sys.argv[1]
     pdf_file = sys.argv[2]
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     post_md = subprocess.check_output(['rmfm'], input=post_md.encode())
     post_html = subprocess.check_output(['kramdown'], input=post_md).decode()
     template_html = subprocess.check_output(['mustache', 'officers.yaml', 'template.mustache']).decode()
-    out_html = template_html.format(content=post_html, date=sys.argv[3])
+    out_html = template_html.format(content=post_html, date=sys.argv[3], next_meeting=sys.argv[4])
     with open('tmp.html', 'w') as f:
         f.write(out_html)
     subprocess.check_output(['wkhtmltopdf', '-s', 'Letter', '--print-media-type', '-B', '0.5in', '-L', '0.5in', '-R', '0.5in', '-T', '0.5in', 'tmp.html', pdf_file])
