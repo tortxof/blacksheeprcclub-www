98314fdddd4ef957cbf0d1de9167de6a89e262e6
Daniel Jones <tortxof@gmail.com>
2017-07-11 18:46:38 -0400

    Reimplement nav with Vue

--- _includes/nav.js
+++ _includes/nav.js
@@ -1,36 +1,18 @@
-function nav_resize() {
-  if (window.innerWidth < 550) {
-    document.getElementById('nav').style.display = 'none'
-    document.getElementById('nav-expand').style.display = 'block'
-    nav_toggle_change_button();
-  } else {
-    document.getElementById('nav').style.display = 'block'
-    document.getElementById('nav-expand').style.display = 'none'
+var vue_nav = new Vue({
+  el: '#nav',
+  data: {
+    small_screen: false,
+    visible: false
+  },
+  methods: {
+    toggle: function() {
+      this.visible = !this.visible
+    }
   }
-}
-
-function nav_toggle_change_button() {
-  var nav_toggle = document.querySelector('#nav-expand > i')
-  if (document.getElementById('nav').style.display === 'block') {
-    nav_toggle.classList.add('fa-times')
-    nav_toggle.classList.remove('fa-bars')
-  } else {
-    nav_toggle.classList.add('fa-bars')
-    nav_toggle.classList.remove('fa-times')
-  }
-}
-
-window.addEventListener('resize', nav_resize)
-
-document.getElementById('nav-expand').addEventListener('click', function() {
-  var nav_el = document.getElementById('nav')
-  if (nav_el.style.display === 'block') {
-    nav_el.style.display = 'none'
-  } else {
-    nav_el.style.display = 'block'
-  }
-  nav_toggle_change_button()
 })
 
+function navResize() {vue_nav.small_screen = window.innerWidth < 550}
 
-nav_resize();
+window.addEventListener('resize', navResize)
+
+navResize()
