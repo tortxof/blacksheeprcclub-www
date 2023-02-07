e8a950ee9c77c83f97d3887c09e73c336064f3c3
Daniel Jones <tortxof@gmail.com>
2023-02-07 12:49:31 -0500

    Use podman for devserver

--- devserver.sh
+++ devserver.sh
@@ -1,6 +1,6 @@
-export JEKYLL_VERSION=3.8
-docker run --rm \
+export JEKYLL_VERSION=4.2.2
+podman run --rm \
   -v $(pwd):/srv/jekyll \
   -p 4000:4000 \
-  jekyll/jekyll:$JEKYLL_VERSION \
+  docker.io/jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve
